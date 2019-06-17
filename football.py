3501bf9b45816806970cb8895ab6e595f21d66f9
saikumar <yerra.sai@mountblue.io>
2019-06-17 14:02:16 +0530

    added weather.py,football.py

--- football.py
+++ football.py
@@ -1,6 +1,6 @@
 import dataExtractor as dE
 import dataAnalyser as dA
-class weather:
+class football:
 
     def __init__(self,file_path):
         self.file_path=file_path
@@ -9,9 +9,8 @@ class weather:
     
     def calculate_min_difference(self,column_1,column_2):
         result=self.extractor.data()
-        result.pop()
         print(self.analyzer.calculate_min_difference(result,column_1,column_2))
 if __name__=='__main__':
-    file_path='weather.dat'
-    k=weather(file_path)
-    k.calculate_min_difference('MxT','MnT')
+    file_path='football.dat'
+    k=football(file_path)
+    k.calculate_min_difference('F','A')
